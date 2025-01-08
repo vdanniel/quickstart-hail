39e9999c74b798d91f0f216fd170b5a3e86f4246
EC2 Default User <ec2-user@ip-172-31-25-85.ap-northeast-2.compute.internal>
2025-01-08 14:30:58 +0000

    update emr-bootstrap-scripts/numpyinstall.sh

--- emr-bootstrap-scripts/numpyinstall.sh
+++ emr-bootstrap-scripts/numpyinstall.sh
@@ -16,8 +16,13 @@ while true; do
 
     if [ "$NODEPROVISIONSTATE" == "SUCCESSFUL" ]; then
         echo "Running my post provision bootstrap"
-        sudo python3 -m pip uninstall numpy -y
-        sudo python3 -m pip install numpy -U
+        sudo python3 -m pip install --upgrade pip
+        sudo python3 -m pip uninstall -y numpy google-auth pyspark hail python37-sagemaker-pyspark  || true
+        sudo python3 -m pip install numpy==1.21.6 -U
+        sudo python3 -m pip install 'google-auth>=1.27.0,<2.0.0'
+        sudo python3 -m pip install 'pyspark==2.3.4'
+        sudo python3 -m pip install 'hail==0.2.105'
+        sudo python3 -m pip install 'python37-sagemaker-pyspark==1.4.1'
         echo '-------BOOTSTRAP COMPLETE-------'
 
         exit
@@ -31,3 +36,4 @@ EOF
 
 chmod +x /var/tmp/fix-bootstap.sh
 nohup /var/tmp/fix-bootstap.sh  2>&1 &
+
